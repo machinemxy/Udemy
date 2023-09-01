3aba12e2c9b6de6f7785e72cd19ae16a2491491a
xueyuan-ma <xueyuan.ma@rakuten.com>
2023-09-01 17:31:21 +0900

    LiveData

--- ViewModelChallenge/app/src/main/java/com/mxy/viewmodelchallenge/MainActivity.kt
+++ ViewModelChallenge/app/src/main/java/com/mxy/viewmodelchallenge/MainActivity.kt
@@ -3,6 +3,7 @@ package com.mxy.viewmodelchallenge
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.mxy.viewmodelchallenge.databinding.ActivityMainBinding
 import java.lang.Exception
@@ -17,13 +18,14 @@ class MainActivity : AppCompatActivity() {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         viewModelFactory = MainActivityViewModelFactory(666)
         viewModel = ViewModelProvider(this, viewModelFactory)[MainActivityViewModel::class.java]
-        binding.resultText.text = viewModel.sum.toString()
+        viewModel.sum.observe(this, Observer {
+            binding.resultText.text = it.toString()
+        })
         binding.addButton.setOnClickListener {
             val strInput = binding.input.text.toString()
             try {
                 val intInput = Integer.parseInt(strInput)
                 viewModel.add(intInput)
-                binding.resultText.text = viewModel.sum.toString()
                 binding.input.text.clear()
             } catch (e: Exception) {
                 println(e.message)
--- ViewModelChallenge/app/src/main/java/com/mxy/viewmodelchallenge/MainActivityViewModel.kt
+++ ViewModelChallenge/app/src/main/java/com/mxy/viewmodelchallenge/MainActivityViewModel.kt
@@ -1,11 +1,14 @@
 package com.mxy.viewmodelchallenge
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainActivityViewModel(startingTotal: Int): ViewModel() {
-    var sum = startingTotal
+    private val _sum = MutableLiveData(startingTotal)
+    val sum: LiveData<Int> get() = _sum
 
     fun add(value: Int) {
-        sum += value
+        _sum.value = _sum.value?.plus(value)
     }
 }
