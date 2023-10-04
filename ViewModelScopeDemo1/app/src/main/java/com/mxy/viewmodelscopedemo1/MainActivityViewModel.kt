291bf5cea8cbc93258762208ab1c64841ff56df4
xueyuan-ma <xueyuan.ma@rakuten.com>
2023-10-04 14:03:38 +0900

    view model scope 1 - done

--- ViewModelScopeDemo1/app/src/main/java/com/mxy/viewmodelscopedemo1/MainActivityViewModel.kt
+++ ViewModelScopeDemo1/app/src/main/java/com/mxy/viewmodelscopedemo1/MainActivityViewModel.kt
@@ -1,4 +1,32 @@
 package com.mxy.viewmodelscopedemo1
 
-class MainActivityViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+
+/* When not using view model scope:
+class MainActivityViewModel: ViewModel() {
+    private val myJob = Job()
+    private val myScope = CoroutineScope(Dispatchers.IO + myJob)
+
+    fun getUserData() {
+        myScope.launch {
+            // ...
+        }
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        myJob.cancel()
+    }
+}
+ */
+
+// When using view model scope:
+class MainActivityViewModel: ViewModel() {
+    fun getUserData() {
+        viewModelScope.launch {
+            // ...
+        }
+    }
 }
