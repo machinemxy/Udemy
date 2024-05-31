cb21fed6a5d1cfadf9580d48be0392bbc40b2390
xueyuan-ma <xueyuan.ma@rakuten.com>
2024-05-31 15:12:01 +0900

    room - dao

---
+++ RoomDemo/app/src/main/java/com/mxy/roomdemo/db/SubscriberDAO.kt
@@ -0,0 +1,27 @@
+package com.mxy.roomdemo.db
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+
+@Dao
+interface SubscriberDAO {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertSubscriber(subscriber: Subscriber): Long
+
+    @Update
+    suspend fun updateSubscriber(subscriber: Subscriber)
+
+    @Delete
+    suspend fun deleteSubscriber(subscriber: Subscriber)
+
+    @Query("DELETE FROM subscriber_data_table")
+    suspend fun deleteAll()
+
+    @Query("SELECT * FROM subscriber_data_table")
+    suspend fun getAllSubscribers(): LiveData<List<Subscriber>>
+}
