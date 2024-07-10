b8560db1e01175f5a6fb189a466b8c6ed834411c
xueyuan-ma <xueyuan.ma@rakuten.com>
2024-07-10 18:09:26 +0900

    retrofit - add get function

---
+++ RetrofitDemo/app/src/main/java/com/mxy/retrofitdemo/AlbumService.kt
@@ -0,0 +1,9 @@
+package com.mxy.retrofitdemo
+
+import retrofit2.Response
+import retrofit2.http.GET
+
+interface AlbumService {
+    @GET(value = "/albums")
+    suspend fun getAlbums(): Response<Albums>
+}
