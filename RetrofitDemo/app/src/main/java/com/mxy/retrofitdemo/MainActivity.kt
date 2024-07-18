00a39bb3ff6f1be3135a34c7340ff1f54baa38de
xueyuan-ma <xueyuan.ma@rakuten.com>
2024-07-18 14:46:02 +0900

    retrofit - get all albums

--- RetrofitDemo/app/src/main/java/com/mxy/retrofitdemo/MainActivity.kt
+++ RetrofitDemo/app/src/main/java/com/mxy/retrofitdemo/MainActivity.kt
@@ -1,10 +1,16 @@
 package com.mxy.retrofitdemo
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+import androidx.lifecycle.liveData
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +22,23 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val textView = findViewById<TextView>(R.id.textView)
+        val retService = RetrofitInstance.getRetrofitInstance().create(AlbumService::class.java)
+        val responseLiveData = liveData {
+            val response = retService.getAlbums()
+            emit(response)
+        }
+        responseLiveData.observe( this, Observer {
+            val albumsList = it.body()?.listIterator()
+            albumsList?.let {
+                while (it.hasNext()) {
+                    val albumsItem = it.next()
+                    textView.append("id: ${albumsItem.id}\n")
+                    textView.append("user id: ${albumsItem.userId}\n")
+                    textView.append("title: ${albumsItem.title}\n\n")
+                }
+            }
+        })
     }
 }
