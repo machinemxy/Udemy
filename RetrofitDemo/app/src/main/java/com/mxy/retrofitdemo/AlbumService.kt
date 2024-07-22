460f0f1459f16da427411bd3255c168b8391a479
xueyuan-ma <xueyuan.ma@rakuten.com>
2024-07-22 14:22:06 +0900

    retrofit - path parameter

--- RetrofitDemo/app/src/main/java/com/mxy/retrofitdemo/AlbumService.kt
+++ RetrofitDemo/app/src/main/java/com/mxy/retrofitdemo/AlbumService.kt
@@ -2,6 +2,7 @@ package com.mxy.retrofitdemo
 
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface AlbumService {
@@ -10,4 +11,7 @@ interface AlbumService {
 
     @GET(value = "/albums")
     suspend fun getAlbumsByUserId(@Query("userId") userId: Int): Response<Albums>
+
+    @GET(value = "/albums/{id}")
+    suspend fun getAlbumById(@Path(value = "id") albumId: Int): Response<AlbumsItem>
 }
