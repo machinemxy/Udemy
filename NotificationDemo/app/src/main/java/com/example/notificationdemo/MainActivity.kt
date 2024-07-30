98b0677ed45f998f26859b3a55dcbe97a49cbfc3
xueyuan-ma <xueyuan.ma@rakuten.com>
2024-07-30 17:29:26 +0900

    notification - send notification

--- NotificationDemo/app/src/main/java/com/example/notificationdemo/MainActivity.kt
+++ NotificationDemo/app/src/main/java/com/example/notificationdemo/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.example.notificationdemo
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private val channelID = "com.mxy.notificationdemo.channel1"
+    private var notificationManager: NotificationManager? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +24,33 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        createNotificationChannel(channelID, "DemoChannel", "this is a demo")
+
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener {
+            displayNotification()
+        }
+    }
+
+    private fun displayNotification() {
+        val notificationId = 1
+        val notification = NotificationCompat.Builder(this, channelID)
+            .setContentTitle("Demo Title")
+            .setContentText("This is a demo notification.")
+            .setSmallIcon(android.R.drawable.ic_dialog_info)
+            .setAutoCancel(true)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .build()
+        notificationManager?.notify(notificationId, notification)
+    }
+
+    private fun createNotificationChannel(id: String, name: String, channelDescription: String) {
+        val importance = NotificationManager.IMPORTANCE_HIGH
+        val channel = NotificationChannel(id, name, importance).apply {
+            description = channelDescription
+        }
+        notificationManager?.createNotificationChannel(channel)
     }
 }
