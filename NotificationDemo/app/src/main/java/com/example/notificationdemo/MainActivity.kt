78376894fac8d2d6c6e4e1e08715edbd7cdeb5e2
xueyuan-ma <xueyuan.ma@rakuten.com>
2024-08-04 17:20:33 +0900

    notification - action buttons

--- NotificationDemo/app/src/main/java/com/example/notificationdemo/MainActivity.kt
+++ NotificationDemo/app/src/main/java/com/example/notificationdemo/MainActivity.kt
@@ -38,10 +38,24 @@ class MainActivity : AppCompatActivity() {
 
     private fun displayNotification() {
         val notificationId = 1
+
         val tapResultIntent = Intent(this, SecondActivity::class.java).apply {
             flags = Intent.FLAG_ACTIVITY_FORWARD_RESULT
         }
         val pendingIntent = PendingIntent.getActivity(this, 0, tapResultIntent, PendingIntent.FLAG_IMMUTABLE)
+
+        val intent2 = Intent(this, DetailsActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_FORWARD_RESULT
+        }
+        val pendingIntent2 = PendingIntent.getActivity(this, 0, intent2, PendingIntent.FLAG_IMMUTABLE)
+        val action2: NotificationCompat.Action = NotificationCompat.Action.Builder(0, "Details", pendingIntent2).build()
+
+        val intent3 = Intent(this, SettingsActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_FORWARD_RESULT
+        }
+        val pendingIntent3 = PendingIntent.getActivity(this, 0, intent3, PendingIntent.FLAG_IMMUTABLE)
+        val action3: NotificationCompat.Action = NotificationCompat.Action.Builder(0, "Settings", pendingIntent3).build()
+
         val notification = NotificationCompat.Builder(this, channelID)
             .setContentTitle("Demo Title")
             .setContentText("This is a demo notification.")
@@ -49,6 +63,8 @@ class MainActivity : AppCompatActivity() {
             .setAutoCancel(true)
             .setPriority(NotificationCompat.PRIORITY_HIGH)
             .setContentIntent(pendingIntent)
+            .addAction(action2)
+            .addAction(action3)
             .build()
         notificationManager?.notify(notificationId, notification)
     }
