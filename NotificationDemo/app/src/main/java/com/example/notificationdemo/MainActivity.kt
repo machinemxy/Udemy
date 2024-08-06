b4729264f6abbb1385ab834189af4ede794a8f71
xueyuan-ma <xueyuan.ma@rakuten.com>
2024-08-06 16:51:22 +0900

    notification - quick reply action

--- NotificationDemo/app/src/main/java/com/example/notificationdemo/MainActivity.kt
+++ NotificationDemo/app/src/main/java/com/example/notificationdemo/MainActivity.kt
@@ -10,12 +10,14 @@ import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationCompat
+import androidx.core.app.RemoteInput
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     private val channelID = "com.mxy.notificationdemo.channel1"
     private var notificationManager: NotificationManager? = null
+    private val keyReply = "key_reply"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,7 +44,15 @@ class MainActivity : AppCompatActivity() {
         val tapResultIntent = Intent(this, SecondActivity::class.java).apply {
             flags = Intent.FLAG_ACTIVITY_FORWARD_RESULT
         }
-        val pendingIntent = PendingIntent.getActivity(this, 0, tapResultIntent, PendingIntent.FLAG_IMMUTABLE)
+        val pendingIntent = PendingIntent.getActivity(this, 0, tapResultIntent, PendingIntent.FLAG_MUTABLE)
+
+        val remoteInput: RemoteInput = RemoteInput.Builder(keyReply).run {
+            setLabel("Insert your name here")
+            build()
+        }
+        val replyAction: NotificationCompat.Action = NotificationCompat.Action.Builder(0, "Reply", pendingIntent)
+            .addRemoteInput(remoteInput)
+            .build()
 
         val intent2 = Intent(this, DetailsActivity::class.java).apply {
             flags = Intent.FLAG_ACTIVITY_FORWARD_RESULT
@@ -65,6 +75,7 @@ class MainActivity : AppCompatActivity() {
             .setContentIntent(pendingIntent)
             .addAction(action2)
             .addAction(action3)
+            .addAction(replyAction)
             .build()
         notificationManager?.notify(notificationId, notification)
     }
--- NotificationDemo/app/src/main/java/com/example/notificationdemo/SecondActivity.kt
+++ NotificationDemo/app/src/main/java/com/example/notificationdemo/SecondActivity.kt
@@ -1,8 +1,10 @@
 package com.example.notificationdemo
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.RemoteInput
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -15,6 +17,16 @@ class SecondActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        receiveInput()
+    }
+
+    private fun receiveInput() {
+        val remoteInput = RemoteInput.getResultsFromIntent(this.intent)
+        remoteInput?.let {
+            val inputString = it.getCharSequence("key_reply").toString()
+            val resultTextView = findViewById<TextView>(R.id.result_text_view)
+            resultTextView.text = inputString
         }
     }
 }
