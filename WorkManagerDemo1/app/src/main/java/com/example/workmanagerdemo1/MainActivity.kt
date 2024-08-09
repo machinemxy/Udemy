d2fddd570d4c497e8200284be5ba1aca841459b8
xueyuan-ma <xueyuan.ma@rakuten.com>
2024-08-09 18:10:45 +0900

    work manager - one time work request

--- WorkManagerDemo1/app/src/main/java/com/example/workmanagerdemo1/MainActivity.kt
+++ WorkManagerDemo1/app/src/main/java/com/example/workmanagerdemo1/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.example.workmanagerdemo1
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.work.OneTimeWorkRequest
+import androidx.work.WorkManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +19,16 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val startButton = findViewById<Button>(R.id.start_button)
+        startButton.setOnClickListener {
+            setOneTimeWorkRequest()
+        }
+    }
+
+    private fun setOneTimeWorkRequest() {
+        val uploadRequest = OneTimeWorkRequest.Builder(UploadWorker::class.java)
+            .build()
+        WorkManager.getInstance(applicationContext)
+            .enqueue(uploadRequest)
     }
 }
