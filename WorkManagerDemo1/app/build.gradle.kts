fb0bece0e06af7b80195afbeb1c8d18a400ad25a
xueyuan-ma <xueyuan.ma@rakuten.com>
2024-08-09 17:56:51 +0900

    work manager - add dependency

--- WorkManagerDemo1/app/build.gradle.kts
+++ WorkManagerDemo1/app/build.gradle.kts
@@ -36,7 +36,7 @@ android {
 }
 
 dependencies {
-
+    implementation(libs.androidx.work.runtime.ktx)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
