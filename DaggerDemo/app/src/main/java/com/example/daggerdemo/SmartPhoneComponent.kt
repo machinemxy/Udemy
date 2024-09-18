23dae2df04b3f4d1d5665e40ba9fdca133dd0353
xueyuan-ma <xueyuan.ma@rakuten.com>
2024-09-18 11:04:43 +0900

    dagger - singleton

--- DaggerDemo/app/src/main/java/com/example/daggerdemo/SmartPhoneComponent.kt
+++ DaggerDemo/app/src/main/java/com/example/daggerdemo/SmartPhoneComponent.kt
@@ -1,7 +1,9 @@
 package com.example.daggerdemo
 
 import dagger.Component
+import javax.inject.Singleton
 
+@Singleton
 @Component(modules = [MemoryCardModule::class, NCBatteryModule::class])
 interface SmartPhoneComponent {
     fun inject(mainActivity: MainActivity)
