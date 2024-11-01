aa0025ab545416644df4742ff3a9db506a20a857
xueyuan-ma <xueyuan.ma@rakuten.com>
2024-11-01 19:44:23 +0900

    TMDBClient - presentation layer 1

--- TMDBClient/app/src/main/java/com/example/tmdbclient/presentation/MovieActivity.kt
+++ TMDBClient/app/src/main/java/com/example/tmdbclient/presentation/MovieActivity.kt
@@ -1,20 +1,26 @@
-package com.example.tmdbclient
+package com.example.tmdbclient.presentation
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
+import com.example.tmdbclient.R
+import com.example.tmdbclient.databinding.ActivityMovieBinding
 
-class MainActivity : AppCompatActivity() {
+class MovieActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMovieBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_movie)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_movie)
     }
 }
