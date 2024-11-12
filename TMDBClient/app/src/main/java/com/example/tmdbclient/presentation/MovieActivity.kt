482a029e4b37ee360b21558d0771501016d9e89b
xueyuan-ma <xueyuan.ma@rakuten.com>
2024-11-12 19:44:15 +0900

    TMDBClient - recyclerView

--- TMDBClient/app/src/main/java/com/example/tmdbclient/presentation/MovieActivity.kt
+++ TMDBClient/app/src/main/java/com/example/tmdbclient/presentation/MovieActivity.kt
@@ -2,6 +2,9 @@ package com.example.tmdbclient.presentation
 
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.LinearLayout
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,6 +12,7 @@ import androidx.core.view.WindowInsetsCompat
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.tmdbclient.R
 import com.example.tmdbclient.databinding.ActivityMovieBinding
 import com.example.tmdbclient.presentation.di.Injector
@@ -19,6 +23,7 @@ class MovieActivity : AppCompatActivity() {
     lateinit var factory: MovieViewModelFactory
     private lateinit var movieViewModel: MovieViewModel
     private lateinit var binding: ActivityMovieBinding
+    private lateinit var adapter: MovieAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,9 +39,29 @@ class MovieActivity : AppCompatActivity() {
             .inject(this)
         movieViewModel = ViewModelProvider(this, factory)
             .get(MovieViewModel::class.java)
-        var responseLiveData = movieViewModel.getMovies()
+        initRecyclerView()
+    }
+
+    private fun initRecyclerView() {
+        binding.movieRecyclerView.layoutManager = LinearLayoutManager(this)
+        adapter = MovieAdapter()
+        binding.movieRecyclerView.adapter = adapter
+        displayPopularMovies()
+    }
+
+    private fun displayPopularMovies() {
+        binding.movieProgressBar.visibility = View.VISIBLE
+        val responseLiveData = movieViewModel.getMovies()
         responseLiveData.observe(this, Observer {
-            Log.i("MYTAG", it.toString())
+            if (it != null) {
+                Log.i("MY_TAG", it.toString())
+                adapter.setList(it)
+                adapter.notifyDataSetChanged()
+                binding.movieProgressBar.visibility = View.GONE
+            } else {
+                binding.movieProgressBar.visibility = View.GONE
+                Toast.makeText(applicationContext, "No data available", Toast.LENGTH_LONG).show()
+            }
         })
     }
 }
---
+++ TMDBClient/app/src/main/java/com/example/tmdbclient/presentation/MovieAdapter.kt
@@ -0,0 +1,44 @@
+package com.example.tmdbclient.presentation
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.tmdbclient.R
+import com.example.tmdbclient.data.model.Movie
+import com.example.tmdbclient.databinding.ListItemBinding
+
+class MovieAdapter(): RecyclerView.Adapter<MovieViewHolder>() {
+    private val movieList = ArrayList<Movie>()
+
+    fun setList(movies: List<Movie>) {
+        movieList.clear()
+        movieList.addAll(movies)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val binding: ListItemBinding = DataBindingUtil.inflate(layoutInflater, R.layout.list_item, parent, false)
+        return MovieViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int {
+        return movieList.size
+    }
+
+    override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
+        holder.bind(movieList[position])
+    }
+}
+
+class MovieViewHolder(val binding: ListItemBinding): RecyclerView.ViewHolder(binding.root) {
+    fun bind(movie: Movie) {
+        binding.titleTextView.text = movie.title
+        binding.descriptionTextView.text = movie.overview
+        val postUrl = "https://image.tmdb.org/t/p/w500${movie.posterPath}"
+        Glide.with(binding.imageView.context)
+            .load(postUrl)
+            .into(binding.imageView)
+    }
+}
