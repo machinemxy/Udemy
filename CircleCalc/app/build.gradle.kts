f4a3d61d3591fd3715173b84af98eac3a7ab718b
xueyuan-ma <xueyuan.ma@rakuten.com>
2024-12-16 17:26:30 +0900

    unit test - Mockito

--- CircleCalc/app/build.gradle.kts
+++ CircleCalc/app/build.gradle.kts
@@ -53,6 +53,7 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     testImplementation("androidx.arch.core:core-testing:$arch_version")
     testImplementation("com.google.truth:truth:1.4.4")
+    testImplementation("org.mockito:mockito-core:3.+")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
