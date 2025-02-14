3c77cc24d5746605266dc16e72bbd374d50ebb22
xueyuan-ma <xueyuan.ma@rakuten.com>
2025-02-14 14:34:49 +0900

    NewsApiClient - remote data source

--- NewsApiClient/app/src/main/java/com/example/newsapiclient/data/api/NewsApiService.kt
+++ NewsApiClient/app/src/main/java/com/example/newsapiclient/data/api/NewsApiService.kt
@@ -1,7 +1,19 @@
 package com.example.newsapiclient.data.api
 
+import com.example.newsapiclient.BuildConfig
 import com.example.newsapiclient.data.model.APIResponse
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface NewsApiService {
-    //suspend fun getTopHeadlines(): Response<APIResponse>
+    @GET("top-headlines")
+    suspend fun getTopHeadlines(
+        @Query("country")
+        country: String,
+        @Query("page")
+        page: Int,
+        @Query("apiKey")
+        apiKey: String = BuildConfig.API_KEY
+    ): Response<APIResponse>
 }
