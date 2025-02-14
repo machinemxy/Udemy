16bd101833c0a4d8a0d90227fa26bbb9a3cb5b0e
xueyuan-ma <xueyuan.ma@rakuten.com>
2025-02-14 13:33:45 +0900

    途中

---
+++ NewsApiClient/app/src/main/java/com/example/newsapiclient/data/api/NewsApiService.kt
@@ -0,0 +1,7 @@
+package com.example.newsapiclient.data.api
+
+import com.example.newsapiclient.data.model.APIResponse
+
+interface NewsApiService {
+    //suspend fun getTopHeadlines(): Response<APIResponse>
+}
