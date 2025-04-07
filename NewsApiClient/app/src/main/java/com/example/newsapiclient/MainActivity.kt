eb37087c796b551848e9c2c5a9f4fbc41f8a03b5
xueyuan-ma <xueyuan.ma@rakuten.com>
2025-04-07 16:44:09 +0900

    NewsApiClient - able to switch Fragment by bottom menu

--- NewsApiClient/app/src/main/java/com/example/newsapiclient/MainActivity.kt
+++ NewsApiClient/app/src/main/java/com/example/newsapiclient/MainActivity.kt
@@ -6,6 +6,10 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavHostController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.newsapiclient.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -21,5 +25,10 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        binding.bnvMenu.setupWithNavController(
+            navHostFragment.navController
+        )
     }
 }
