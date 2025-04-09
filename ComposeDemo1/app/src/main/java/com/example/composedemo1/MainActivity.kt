8df23ee95c3f8d36d9d2eb677852eaba2d613759
xueyuan-ma <xueyuan.ma@rakuten.com>
2025-04-09 14:58:28 +0900

    Compose - modifiers and parameters

--- ComposeDemo1/app/src/main/java/com/example/composedemo1/MainActivity.kt
+++ ComposeDemo1/app/src/main/java/com/example/composedemo1/MainActivity.kt
@@ -4,13 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.composedemo1.ui.theme.ComposeDemo1Theme
 
 class MainActivity : ComponentActivity() {
@@ -21,8 +30,7 @@ class MainActivity : ComponentActivity() {
             ComposeDemo1Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                        name = "Android"
                     )
                 }
             }
@@ -31,10 +39,19 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun Greeting(name: String) {
     Text(
         text = "Hello $name!",
-        modifier = modifier
+        fontSize = 32.sp,
+        fontWeight = FontWeight.Bold,
+        color = Color.Red,
+        textAlign = TextAlign.Center,
+        modifier = Modifier
+            .background(color = Color.Yellow)
+            .border(2.dp, color = Color.Green)
+            .padding(10.dp)
+            .fillMaxWidth(0.5f)
+            .fillMaxHeight(0.3f)
     )
 }
 
