e514f005f879d8c4748190634be0278bd9809fa4
xueyuan-ma <xueyuan.ma@rakuten.com>
2025-04-14 17:38:42 +0900

    Compose - Box

--- ComposeDemo1/app/src/main/java/com/example/composedemo1/MainActivity.kt
+++ ComposeDemo1/app/src/main/java/com/example/composedemo1/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.composedemo1
 
+import android.R
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -7,11 +8,14 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -19,11 +23,13 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.composedemo1.ui.theme.ComposeDemo1Theme
+import java.nio.file.WatchEvent
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,8 +44,7 @@ class MainActivity : ComponentActivity() {
                 verticalArrangement = Arrangement.SpaceEvenly
             ) {
                 Greeting("AB")
-                Greeting("CDEF")
-                Greeting("G")
+                BoxDemo()
             }
         }
     }
@@ -58,6 +63,31 @@ fun Greeting(name: String) {
             .border(2.dp, color = Color.Green)
             .padding(10.dp)
     )
+}
+
+@Composable
+fun BoxDemo() {
+    Box(
+        modifier = Modifier
+            .background(color = Color.Green)
+            .size(180.dp, 300.dp)
+    ) {
+        Box(
+            modifier = Modifier
+                .background(color = Color.Yellow)
+                .size(125.dp, 100.dp)
+                .align(Alignment.TopCenter)
+        )
+
+        Text(
+            text = "Hi",
+            style = MaterialTheme.typography.headlineSmall,
+            modifier = Modifier
+                .background(color = Color.White)
+                .size(90.dp, 50.dp)
+                .align(Alignment.BottomCenter)
+        )
+    }
 }
 
 @Preview(showBackground = true)
