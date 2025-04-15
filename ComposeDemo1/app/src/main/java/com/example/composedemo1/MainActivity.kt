9ef9ce5643576361e80226b2223f686a0c055087
xueyuan-ma <xueyuan.ma@rakuten.com>
2025-04-15 17:19:24 +0900

    Compose - Button

--- ComposeDemo1/app/src/main/java/com/example/composedemo1/MainActivity.kt
+++ ComposeDemo1/app/src/main/java/com/example/composedemo1/MainActivity.kt
@@ -1,35 +1,36 @@
 package com.example.composedemo1
 
-import android.R
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CutCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.composedemo1.ui.theme.ComposeDemo1Theme
-import java.nio.file.WatchEvent
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -45,6 +46,7 @@ class MainActivity : ComponentActivity() {
             ) {
                 Greeting("AB")
                 BoxDemo()
+                ButtonDemo()
             }
         }
     }
@@ -70,7 +72,7 @@ fun BoxDemo() {
     Box(
         modifier = Modifier
             .background(color = Color.Green)
-            .size(180.dp, 300.dp)
+            .size(180.dp, 200.dp)
     ) {
         Box(
             modifier = Modifier
@@ -87,6 +89,25 @@ fun BoxDemo() {
                 .size(90.dp, 50.dp)
                 .align(Alignment.BottomCenter)
         )
+    }
+}
+
+@Composable
+fun ButtonDemo() {
+    val context = LocalContext.current
+    Button(
+        onClick = {
+            Toast.makeText(context, "Clicked on Button", Toast.LENGTH_SHORT).show()
+        },
+        contentPadding = PaddingValues(16.dp),
+        border = BorderStroke(2.dp, Color.Black),
+        colors = ButtonDefaults.textButtonColors(
+            contentColor = Color.White,
+            containerColor = Color.DarkGray
+        ),
+        shape = CutCornerShape(10.dp)
+    ) {
+        Text("Add to Cart")
     }
 }
 
