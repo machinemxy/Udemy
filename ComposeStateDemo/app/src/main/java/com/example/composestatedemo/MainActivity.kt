ee966bf64c693ca78349d7b26e1903c71aeeab6a
xueyuan-ma <xueyuan.ma@rakuten.com>
2025-05-16 16:10:08 +0900

    ComposeStateDemo - rememberSavable

--- ComposeStateDemo/app/src/main/java/com/example/composestatedemo/MainActivity.kt
+++ ComposeStateDemo/app/src/main/java/com/example/composestatedemo/MainActivity.kt
@@ -13,12 +13,11 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.composestatedemo.ui.theme.ComposeStateDemoTheme
 
 class MainActivity : ComponentActivity() {
@@ -33,7 +32,7 @@ class MainActivity : ComponentActivity() {
                     verticalArrangement = Arrangement.Center,
                     horizontalAlignment = Alignment.CenterHorizontally
                 ) {
-                    var count by remember { mutableIntStateOf(0) }
+                    var count by rememberSaveable { mutableIntStateOf(0) }
                     MyButton(count) { count = it + 1 }
                 }
 
