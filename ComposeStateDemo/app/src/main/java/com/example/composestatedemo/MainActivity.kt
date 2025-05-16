5cbf81182e8a623265980f38a2f9d0619e2766e5
xueyuan-ma <xueyuan.ma@rakuten.com>
2025-05-16 15:53:42 +0900

    ComposeStateDemo - property delegates

--- ComposeStateDemo/app/src/main/java/com/example/composestatedemo/MainActivity.kt
+++ ComposeStateDemo/app/src/main/java/com/example/composestatedemo/MainActivity.kt
@@ -11,8 +11,10 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -43,13 +45,13 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MyButton() {
     val context = LocalContext.current
-    val count = remember { mutableIntStateOf(0) }
+    var count by remember { mutableIntStateOf(0) }
     Button(
         onClick = {
-            count.intValue = count.intValue + 1
-            Toast.makeText(context, "Count is ${count.intValue}", Toast.LENGTH_SHORT).show()
+            count += 1
+            Toast.makeText(context, "Count is ${count}", Toast.LENGTH_SHORT).show()
         }
     ) {
-        Text("Count is ${count.intValue}")
+        Text("Count is ${count}")
     }
 }
