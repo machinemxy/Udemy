94c79a76ddd240de3026a9f79cd0bf04779c3188
xueyuan-ma <xueyuan.ma@rakuten.com>
2025-05-16 15:43:36 +0900

    ComposeStateDemo - remember state

--- ComposeStateDemo/app/src/main/java/com/example/composestatedemo/MainActivity.kt
+++ ComposeStateDemo/app/src/main/java/com/example/composestatedemo/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -38,12 +39,11 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-val count = mutableIntStateOf(0)
-
 @Preview(name = "MyButton")
 @Composable
 fun MyButton() {
     val context = LocalContext.current
+    val count = remember { mutableIntStateOf(0) }
     Button(
         onClick = {
             count.intValue = count.intValue + 1
