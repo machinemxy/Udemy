8b5cb83641eb3ce546ae3d7e71267cdc1dc967a4
xueyuan-ma <xueyuan.ma@rakuten.com>
2025-06-03 17:06:13 +0900

    FlowDemo - flow with compose

--- FlowDemo/app/src/main/java/com/example/flowdemo/MainActivity.kt
+++ FlowDemo/app/src/main/java/com/example/flowdemo/MainActivity.kt
@@ -9,39 +9,32 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.flowdemo.ui.theme.FlowDemoTheme
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.flow
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        val myFlow = flow<Int> {
+            for (i in 1..100) {
+                emit(i)
+                delay(1000L)
+            }
+        }
+
         setContent {
+            val currentValue = myFlow.collectAsState(initial = 1).value
             FlowDemoTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Text("Hello $currentValue", modifier = Modifier.padding(innerPadding))
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FlowDemoTheme {
-        Greeting("Android")
     }
 }
