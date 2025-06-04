d5441e046c9b0488c7f25a940c9c564b49545893
xueyuan-ma <xueyuan.ma@rakuten.com>
2025-06-04 16:52:17 +0900

    FlowDemo - add view model

--- FlowDemo/app/src/main/java/com/example/flowdemo/MainActivity.kt
+++ FlowDemo/app/src/main/java/com/example/flowdemo/MainActivity.kt
@@ -8,28 +8,19 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.flowdemo.ui.theme.FlowDemoTheme
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.flow
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
-        val myFlow = flow<Int> {
-            for (i in 1..100) {
-                emit(i)
-                delay(1000L)
-            }
-        }
-
         setContent {
-            val currentValue = myFlow.collectAsState(initial = 1).value
+            val viewModel = viewModel<MyViewModel>()
+            val currentValue = viewModel.myFlow.collectAsState(initial = 1).value
             FlowDemoTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Text("Hello $currentValue", modifier = Modifier.padding(innerPadding))
